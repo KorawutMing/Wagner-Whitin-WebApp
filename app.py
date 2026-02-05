62e835d06556d2b187aa129e3e4f47af7cf04a0c
KorawutMing <6432004421@student.chula.ac.th>
2026-02-05 14:30:54 +0700

    update icon

--- app.py
+++ app.py
@@ -6,12 +6,19 @@ import io
 import base64
 from matplotlib.figure import Figure
 import networkx as nx
+import os
+from flask import send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path),
+                               'icon.png', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/compute', methods=['POST'])
 def compute():
